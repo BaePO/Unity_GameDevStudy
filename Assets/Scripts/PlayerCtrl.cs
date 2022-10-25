942ca8d1a4a9720f5af49e020e73bcd998b11814
BaePO <96255741+BaePO@users.noreply.github.com>
2022-10-25 18:27:35 +0900

    1025 Chapter 6 Finish

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -9,11 +9,27 @@ public class PlayerCtrl : MonoBehaviour
     // Animation 컴포넌트를 저장할 변수
     private Animation anim;
 
+    // 이동 속력 변수 (public으로 선언돼 인스펙터 뷰에 노출됨)
     public float moveSpeed = 10.0f;
+    // 회전 속도 변수
     public float turnSpeed = 80.0f;
 
+    // 초기 생명 값
+    private readonly float initHp = 100.0f;
+    // 현재 생명 값
+    public float currHp;
+
+    // 델리게이트 선언
+    public delegate void PlayerDieHandler();
+    // 이벤트 선언
+    public static event PlayerDieHandler OnPlayerDie;
+
+    // Start는 코루틴으로 실행 가능하다 -> 0.3f 대기하여 turnSpeed를 적용함으로써 처음 시작할 때 넘어온 마우스의 불규칙한 값(노이즈, 쓰레깃값)을 적용하지 않고 안정적인 값이 넘어올 때까지 대기했다가 로직 실행
     IEnumerator Start()
     {
+        // HP 초기화
+        currHp = initHp;
+         
         // 컴포넌트를 추출하여 변수에 대입
         tr = this.gameObject.GetComponent<Transform>(); // Generic : 데이터 타입(형)을 미리 정해놓는 것 <Transform>으로 Parameter()를 받는다.
         // 마찬가지로 this.gameObject는 생략가능하지만 TypeScript 개발에서는 불가능하므로 사용하는 습관을 기르자.
@@ -80,5 +96,45 @@ public class PlayerCtrl : MonoBehaviour
         {
             anim.CrossFade("Idle", 0.25f);
         }
+    }
+
+    // 충돌한 Collider의 IsTrigger 옵션이 체크됐을 때 발생
+    private void OnTriggerEnter(Collider coll)
+    {
+        // 충돌한 Collider가 몬스터의 PUNCH이면 Player의 HP 차감
+        if (currHp >= 0.0f && coll.CompareTag("PUNCH"))
+        {
+            currHp -= 10.0f;
+            Debug.Log($"Player hp = {currHp / initHp}");
+
+            // Player의 생명이 0 이하이면 사망 처리
+            if (currHp <= 0.0f)
+            {
+                PlayerDie();
+            }
+        }
+    }
+
+    // Player 사망 처리
+    void PlayerDie()
+    {
+        Debug.Log("Player Die !");
+
+        /*
+        // MONSTER 태그를 가진 모든 게임오브젝트를 찾아옴
+        GameObject[] monsters = GameObject.FindGameObjectsWithTag("MONSTER");
+
+        // 모든 몬스터의 OnPlayerDie 함수를 순차적으로 호출
+        foreach(GameObject monster in monsters)
+        {
+            // monster 오브젝트에 있는 OnPlayerDie라는 함수를 호출, 없더라도 없다는 메시지를 반환받지 않겠음.
+            monster.SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
+        }
+        
+        => foreach로 순회하면서 SendMessage를 하면 메모리 사용 측면이나 구동 속도 측면에서 ↓
+        */
+
+        // 주인공 사망 이벤트 호출(발생)
+        OnPlayerDie();
     }
 }
